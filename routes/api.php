c6ae0af3f1fca5d3fbce6d56f62a109832982d85
wassim <wassim99f@gmail.com>
2023-12-06 22:48:26 +0200

    create the CRUD operation routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactInformationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/list-contacts', [ContactInformationController::class, 'index']);
+Route::post('/store-contacts', [ContactInformationController::class, 'store']);
+Route::post('/update-contacts/{id}', [ContactInformationController::class, 'update']);
+Route::delete('/delete-contacts/{id}', [ContactInformationController::class, 'destroy']);
