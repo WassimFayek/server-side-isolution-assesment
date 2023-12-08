b09fccf7e42e272a513f972b0938822c7aeac465
wassim <wassim99f@gmail.com>
2023-12-08 22:06:50 +0200

    import contactRequest and use it in store and update function

--- app/Http/Controllers/Api/ContactInformationController.php
+++ app/Http/Controllers/Api/ContactInformationController.php
@@ -2,30 +2,31 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Models\ContactInformation;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ContactRequest;
 use Illuminate\Validation\ValidationException;
 
 class ContactInformationController extends Controller
 {
+    /**
+     * @return JsonResponse
+     */
     public function index(): JsonResponse
     {
         $contacts = ContactInformation::all();
         return response()->json($contacts);
     }
 
-    public function store(Request $request): JsonResponse
+    /**
+     * @param ContactRequest $request
+     * @return JsonResponse
+     */
+    public function store(ContactRequest $request): JsonResponse
     {
         try {
-            $validatedData = $request->validate([
-                'first_name' => 'required',
-                'last_name' => 'required',
-                'email' => 'required|email|unique:contact_information',
-                'phone_number' => 'nullable|numeric',
-            ]);
-
+            $validatedData = $request->validated();
             $contact = ContactInformation::create($validatedData);
 
             return response()->json($contact, 201);
@@ -35,17 +36,16 @@ class ContactInformationController extends Controller
         }
     }
 
-    public function update(Request $request, $id): JsonResponse
+    /**
+     * @param ContactRequest $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function update(ContactRequest $request, $id): JsonResponse
     {
         try {
-            $validatedData = $request->validate([
-                'first_name' => 'required',
-                'last_name' => 'required',
-                'email' => 'required|email|unique:contact_information,email,' . $id,
-                'phone_number' => 'nullable|numeric',
-            ]);
-
             $contact = ContactInformation::findOrFail($id);
+            $validatedData = $request->validated();
             $contact->update($validatedData);
 
             return response()->json($contact, 200);
@@ -58,6 +58,10 @@ class ContactInformationController extends Controller
         }
     }
 
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
     public function destroy($id): JsonResponse
     {
         $contact = ContactInformation::find($id);
